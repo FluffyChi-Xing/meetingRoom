c6a16c1be4ea60c1aaf078cc31bf385136b93cd7
FluffyChi-Xing <121754413+FluffyChi-Xing@users.noreply.github.com>
2024-04-22 20:35:50 +0800

    第三次提交

--- src/views/Login/LoginPage.vue
+++ src/views/Login/LoginPage.vue
@@ -2,6 +2,7 @@
 import { ref,reactive } from "vue";
 import {ElMessage} from "element-plus";
 import { useRouter} from "vue-router";
+import axios from "axios";
 //active
 const active = ref('first')
 //login-form
@@ -30,6 +31,92 @@ const login = () => {
     type: "warning",
     message: '用户名或密码不能为空'
   })
+}
+//register
+const registerForm = reactive({
+  username: '',
+  password: '',
+  nickName: '',
+  email: '',
+  captcha: ''
+})
+//submit text
+const subText = ref('发送')
+//is disabled
+const isDisabled = ref(false)
+
+//captcha function
+const captcha = () => {
+  if (registerForm.email) {
+    if (registerForm.username && registerForm.password) {
+      const email = registerForm.email
+      isDisabled.value = true
+      let tick = 60
+        let timer = setInterval(() => {
+          if (tick > 0) {
+            tick -= 1
+            subText.value = `重新发送(${tick})`
+          } else {
+            clearTimeout(timer)
+            isDisabled.value = false
+            subText.value = '发送'
+          }
+        },1000)
+      axios.get('http://localhost:3000/user/captcha',{
+        params: {
+          address: email
+        }
+      }).then((res) => {
+        ElMessage({
+          type: "success",
+          message: res.data.message
+        })
+      }).catch((e) => {
+        ElMessage({
+          type: "warning",
+          message: '错误'
+        })
+        console.log(e)
+      })
+    } else {
+      ElMessage({
+        type: "warning",
+        message: '用户名和密码不可为空'
+      })
+    }
+  } else {
+    ElMessage({
+      type: "warning",
+      message: '邮箱不可为空'
+    })
+  }
+}
+//registerSubmit
+const registerSubmit = () => {
+  if (registerForm.username && registerForm.password && registerForm.nickName && registerForm.email && registerForm.captcha) {
+    axios.post('http://localhost:3000/user/register',{
+      username: registerForm.username,
+      password: registerForm.password,
+      nickName: registerForm.nickName,
+      email: registerForm.email,
+      captcha: registerForm.captcha
+    },{
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }).then((res) => {
+      ElMessage({
+        type: "success",
+        message: res.data.message
+      })
+    }).catch((e) => {
+      ElMessage({
+        type: "warning",
+        message: '错误'
+      })
+      console.log(e)
+    })
+  }
 }
 </script>
 
@@ -63,8 +150,34 @@ const login = () => {
               </div>
             </el-tab-pane>
             <el-tab-pane label="注册" name="second">
-              <div class="w-full h-[450px] relative block bg-red-950">
-
+              <div class="w-full h-auto relative block">
+                <el-form
+                    label-width="auto"
+                    v-model="registerForm"
+                    class="w-full h-full relative block"
+                >
+                  <el-form-item label="用户名">
+                    <el-input placeholder="请输入用户名" v-model="registerForm.username" clearable prefix-icon="User" />
+                  </el-form-item>
+                  <el-form-item label="密码">
+                    <el-input placeholder="请输入密码" prefix-icon="Lock" v-model="registerForm.password" type="password" clearable show-password />
+                  </el-form-item>
+                  <el-form-item label="别名">
+                    <el-input v-model="registerForm.nickName" placeholder="请输入别名" clearable />
+                  </el-form-item>
+                  <el-form-item label="邮箱">
+                    <el-input v-model="registerForm.email" clearable placeholder="请输入邮箱" />
+                  </el-form-item>
+                  <el-form-item label="验证码">
+                    <div class="w-full h-auto relative flex">
+                      <el-input style="width: 240px" v-model="registerForm.captcha" maxlength="6" show-word-limit clearable placeholder="请输入验证码" />
+                      <el-button type="primary" class="ml-4" @click="captcha" :disabled="isDisabled">{{ subText }}</el-button>
+                    </div>
+                  </el-form-item>
+                  <el-form-item label="注册">
+                    <el-button type="primary" class="w-full h-auto relative block" @click="registerSubmit">注册</el-button>
+                  </el-form-item>
+                </el-form>
               </div>
             </el-tab-pane>
           </el-tabs>
